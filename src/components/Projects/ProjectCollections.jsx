0f6eb1dfa16d0d3fc880badf57133de34c6b0fcf
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-11-23 07:52:18 +0000

    update projexts

--- src/components/Projects/ProjectCollections.jsx
+++ src/components/Projects/ProjectCollections.jsx
@@ -1,14 +1,14 @@
 export const Collections = [
     {
         id:1,
-        videoUrl:"https://www.youtube.com/embed/uHYOwws2BP8"
+        videoUrl:"https://www.youtube.com/embed/4YWx5DkBfH4?si=j0nJTOjyE15WtaVI"
     },
     {
         id:2,
-        videoUrl:"https://www.youtube.com/embed/0BSL1kDs0JU"
+        videoUrl:"https://www.youtube.com/embed/LaJ0Gmp_Sik?si=LJBB59H47WZiiezi"
     },
     {
         id:3,
-        videoUrl:"https://www.youtube.com/embed/wMRYPE0f7rk"
+        videoUrl:"https://drive.google.com/file/d/1TO59ZuZivDENubR4Buk-2A_bkyGf-Nof/preview"
     }
 ]
